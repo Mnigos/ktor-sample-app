6df0d0a6af43e2a0d69c7396165e0f6c236b9779
MoneyIgos <makowskiigor@gmail.com>
2021-02-26 12:18:40 +0100

    feat(quiz): add questions route

--- src/QuizRoute.kt
+++ src/QuizRoute.kt
@@ -1,2 +1,20 @@
 package com.example
 
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import java.util.*
+
+fun Routing.quiz() {
+    route("/quiz") {
+        get("/questions") {
+            val questions: Array<Question> = arrayOf(
+                Question("Do you like hamburgers?", arrayOf("yes", "no")),
+                Question("Have you ever been in Poland?", arrayOf("yes", "no")),
+                Question("Do you learn javascript", arrayOf("yes", "no")),
+            )
+        }
+    }
+}
